e7618667ce0aa124010bcac3103bfbeacc8f3db4
Sanjit4066 <133080514+Sanjit4066@users.noreply.github.com>
2025-12-01 12:08:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+function App() {  //function or component name must start with Capital letter
+  return (
+    // for using multiple h1 tags we need them to write inside div tags
+    <div>
+      <Header />
+      <h1>Hello Everyone this is project alpha</h1>
+      <h1>Learn from Basics</h1>
+    </div>
+  )
+}
+export default App
